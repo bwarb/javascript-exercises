2bbf53c9792a2eff43f621a121c6e473a122aec3
bwarb <benburtness@gmail.com>
2025-10-04 11:15:28 -0600

    Complete The Odin Project Javascript exercises 6-11
    Updated Javascript exercise files for loops and
    arrays lesson.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+  const sort = Array.from(arguments).sort((a, b) => a - b);
+  if (!(first > 0) || !(last > 0) || !(Number.isInteger(first)) || !(Number.isInteger(last))) {
+    return("ERROR");
+  }
+  let sum = 0;
+  for (i = sort[0]; i <= sort[1]; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(Number.isInteger(year/100)) {
+        return Number.isInteger(year/400);
+    }
+    else {
+        return Number.isInteger(year/4);
+    }
 };
 
 // Do not edit below this line
